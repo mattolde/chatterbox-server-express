f652a43aaee342d9a014d60e08f667b5dfdfc624
Matthew Olde <mattolde@gmail.com>
2014-05-06 19:59:04 -0700

    Completed basic REST API

--- app/app.js
+++ app/app.js
@@ -6,13 +6,31 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+
+// Enables CORS
+var enableCORS = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+ 
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+ 
+// enable CORS!
+app.use(enableCORS);
 
 app.use(favicon());
 app.use(logger('dev'));
@@ -22,7 +40,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+var messages = [];
+
+var findRoomMsgs = function(room, messages){
+  var roomMsgs = [];
+
+  for(var i = 0; i < messages.length; i++){
+    if(messages[i].roomname === room){
+      roomMsgs.push(messages[i]);
+    }
+  }
+  return roomMsgs;
+}
+
+router.get('/classes/messages', function(req, res) {
+  res.json({results: messages});
+});
+
+router.post('/classes/messages', function(req, res){
+  var username = req.param('username');
+  var text = req.param('text');
+  var roomname = req.param('roomname');
+
+  messages.push({username: username, text: text, roomname: roomname});
+
+  res.json({created: true});
+});
+
+router.get('/classes/:room', function(req, res){
+  var roomname = req.param('room');
+  var msgs = findRoomMsgs(roomname, messages);
+  res.json({results: msgs});
 });
 
 module.exports = router;
